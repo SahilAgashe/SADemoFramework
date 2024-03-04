1be2b1c2fa716826a2a095936682ee8cb9f69f57
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-03-04 11:16:24 +0530

    add framework

---
+++ SADemoFramework/SADemoFramework/SADemo.swift
@@ -0,0 +1,17 @@
+//
+//  SADemo.swift
+//  SADemoFramework
+//
+//  Created by SAHIL AMRUT AGASHE on 04/03/24.
+//
+
+import Foundation
+
+public class SADemo {
+    
+    public init() {}
+    private let introString = "Hi, I am Sahil Amrut Agashe. This is my demo framework, I created this for learning purpose."
+    public func main() -> String {
+        return introString
+    }
+}
---
+++ SADemoFramework/SADemoFramework/SADemoFramework.h
@@ -0,0 +1,18 @@
+//
+//  SADemoFramework.h
+//  SADemoFramework
+//
+//  Created by SAHIL AMRUT AGASHE on 04/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SADemoFramework.
+FOUNDATION_EXPORT double SADemoFrameworkVersionNumber;
+
+//! Project version string for SADemoFramework.
+FOUNDATION_EXPORT const unsigned char SADemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SADemoFramework/PublicHeader.h>
+
+
